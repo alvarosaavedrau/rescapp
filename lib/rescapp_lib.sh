5d96bac924feb6154bff8f148a3f500db1396770
alvarosaavedrau <cloud@alvarosaavedra.es>
2025-01-08 14:53:32 +0100

    Update rescatux web page link

--- lib/rescapp_lib.sh
+++ lib/rescapp_lib.sh
@@ -1788,7 +1788,7 @@ function rtux_Dbus_End () {
 
 # Rescatux lib main variables
 
-RESCATUX_URL="https://www.rescatux.org/"
+RESCATUX_URL="https://www.supergrubdisk.org/rescatux/"
 RESCATUX_IRC_URL="ircs://irc.libera.chat:6697/rescatux"
 RESCATUX_PASTEBIN_URL="https://paste.debian.net"
 RESC_USER_IRC_PREFIX="resc_"
